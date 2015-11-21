bc68dbfe4b3114f2cee9526ec6226f4f0d69029c
farhanachowdhury <farhanaperveen@yahoo.com>
2015-11-20 23:19:50 -0500

    version 2
    FPS 3D

--- Lesson_seven/Assets/Imported Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Lesson_seven/Assets/Imported Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -28,6 +28,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
+        public Animator rifleAnimator;
+
         private Camera m_Camera;
         private bool m_Jump;
         private float m_YRotation;
@@ -206,6 +208,16 @@ namespace UnityStandardAssets.Characters.FirstPerson
             float vertical = CrossPlatformInputManager.GetAxis("Vertical");
 
             bool waswalking = m_IsWalking;
+
+//play aim animation when right mouse clicked and hold
+            if (CrossPlatformInputManager.GetButton("Fire2"))
+            {
+                this.rifleAnimator.SetBool("Aim", true);
+            }
+            else
+            {
+                this.rifleAnimator.SetBool("Aim", false);
+            }
 
 #if !MOBILE_INPUT
             // On standalone builds, walk/run speed is modified by a key press.
---
+++ Lesson_seven/Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+
+
+public class PlayerShooting : MonoBehaviour {
+
+    // PUBLIC INSTANCE VARIABLES - ACCESSIBLE ON INSPECTOR
+    public ParticleSystem muzzleflash;
+    public GameObject impact;
+    public Animator rifleAnimator;
+    public AudioSource bulletFireSound;
+
+    // PRIVATE INSTANCE VARIABLES
+    private GameObject[] _impacts;
+    private int _currentImpact = 0;
+    private int _maxImpacts = 5;
+
+    private bool _shooting = false;
+
+	// Use this for initialization
+	void Start () {
+        // object pool for the impact particle system
+        this._impacts = new GameObject[this._maxImpacts];
+        for (int impactCount = 0; impactCount < this._maxImpacts; impactCount++)
+        {
+            this._impacts[impactCount] = (GameObject)Instantiate(this.impact);
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            this.muzzleflash.Play();
+            this.bulletFireSound.Play();
+            rifleAnimator.SetTrigger("Fire");
+            this._shooting = true;
+        }
+        if (CrossPlatformInputManager.GetButtonUp("Fire1"))
+        {
+            this._shooting = false;
+        }
+	
+	}
+}
